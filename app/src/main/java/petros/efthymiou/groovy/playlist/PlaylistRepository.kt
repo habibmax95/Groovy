29f38ac3d091e077ec93b65c100800d1fe83c07f
Habibur Rahman <habibur.rahman@assaabloy.com>
2023-04-12 08:45:36 +0200

    Service layer completed

--- app/src/main/java/petros/efthymiou/groovy/playlist/PlaylistRepository.kt
+++ app/src/main/java/petros/efthymiou/groovy/playlist/PlaylistRepository.kt
@@ -1,10 +1,13 @@
 package petros.efthymiou.groovy.playlist
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
-class PlaylistRepository {
+class PlaylistRepository(
+    private val playlistService: PlaylistService
+) {
     suspend fun getPlaylists(): Flow<Result<List<Playlist>>> {
-        TODO("Not yet implemented")
+        return playlistService.fetchPlaylists()
     }
 
 }
